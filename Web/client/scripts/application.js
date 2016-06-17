e91339b839bffe9d07d566d21e3447994207359b
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-06-17 13:10:35 -0400

    Committing function to return character count map

--- Web/client/scripts/application.js
+++ Web/client/scripts/application.js
@@ -34,13 +34,18 @@ function checkWord(solution, guess){
     }
 
     var count = countChars(guess);
-
-
 };
 
-function countChars(word){
-    var letters = word.split('');
-    var count = []
+function getFrequency(string) {
+    var freq = {};
+    for (var i=0; i<string.length;i++) {
+        var character = string.charAt(i);
+        if (freq[character]) {
+           freq[character]++;
+        } else {
+           freq[character] = 1;
+        }
+    }
 
-    return count;
+    return freq;
 };
