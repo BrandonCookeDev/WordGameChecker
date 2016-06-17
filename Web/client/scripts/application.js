f41bf74520141a24db159f47832d7790f6b1a306
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-06-17 12:49:37 -0400

    Guess click going into the checkword function. Logic still needs to be worked out.

--- Web/client/scripts/application.js
+++ Web/client/scripts/application.js
@@ -10,12 +10,37 @@ app.controller('wordCheckerCtrl', function($scope){
     $scope.guessWord    = "";
 
     $scope.submitSolution = function(){
-        $scope.solutionWord = $('#solutionTextbox').val()
+        $scope.solutionWord = $('#solutionTextbox').val();
         $('#solutionTextbox').val('##########');
         $('#solutionTextbox').attr('disabled', true);
     };
 
     $scope.submitGuess = function(){
+        $scope.guessWord = $('#guessTextbox').val();
+        $('#guessTextbox').val("");
 
+        var results = checkWord($scope.solutionWord, $scope.guessWord);
     };
+
+
 });
+
+function checkWord(solution, guess){
+    //Manipulate This
+    var results = {
+        guessWord: '',
+        numCorrect: 0,
+        posCorrect: 0
+    }
+
+    var count = countChars(guess);
+
+
+};
+
+function countChars(word){
+    var letters = word.split('');
+    var count = []
+
+    return count;
+};
